aaa841cf29b3cf450a76c1862ca6ff9aa18c7b81
₿ in the moment <aro.fady@gmail.com>
2020-01-18 19:20:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,39 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+function setupDB() {
+
+    let db = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    });
+
+    db.connect((err) => {
+        if (err) {
+            console.log('db error', err);
+            setTimeout(setupDB, 2000);
+        } else {
+            console.log('Connected to database');
+        }
+    });
+
+    db.on('error', function (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            setupDB();
+        } else {
+            console.log('db error', err);
+            throw err;
+        }
+    });
+
+    global.db = db;
+}
+
 const core = require('./core/app');
+
+setupDB();
 
 express()
     .use(express.static(path.join(__dirname, 'src')))
